33545a52b009237d8caa9cb4fe9528b614686980
Danny <dannypen@gmail.com>
2020-02-17 15:52:51 +0000

    Improved reading block

--- editing.py
+++ editing.py
@@ -1,15 +1,11 @@
 with open("teams.txt","r") as teams:
-    temp = []
-    teams.seek(0)
-    for i in range(5):
-        if i == 0:
-            teams.readline()
-        else:
-            temp.append(teams.readline())
+    temp = teams.readlines()
+
 with open("teams.txt","w") as teams:
     teams.write("This is a new line \n")
-    for i in range(len(temp)):
+    for i in range(1,len(temp)):
         teams.write(temp[i])
+
 
 with open("teams.txt","r") as teams:
     for i in range(5):
