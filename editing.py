f1c9e49d6255c02a85e69cb92ff9a5c7d52b2bcb
Danny <dannypen@gmail.com>
2020-02-17 16:00:22 +0000

    Added comments

--- editing.py
+++ editing.py
@@ -1,12 +1,14 @@
+# Reading the orignal file and saving each line to a list
 with open("teams.txt","r") as teams:
     temp = teams.readlines()
 
+# Wiping the file then adding our new line then re-adding the original lines
 with open("teams.txt","w") as teams:
     teams.write("This is a new line \n")
     for i in range(1,len(temp)):
         teams.write(temp[i])
 
-
+# Reading the file line by line
 with open("teams.txt","r") as teams:
     for i in range(5):
         print("Line %d: " %(i+1), teams.readline())
