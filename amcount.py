a227f4bd82471268a49ea63fecaee2b0f2abc939
Danny <dannypen@gmail.com>
2020-02-17 17:14:33 +0000

    Added file

---
+++ amcount.py
@@ -0,0 +1,10 @@
+def amCounter(x):
+    x.lower()
+    count = 0
+    y = x.split(" ")
+    for i in y:
+        if i == "am":
+            count += 1
+    return count
+
+print(amCounter("A very am string with ams in it am"))
