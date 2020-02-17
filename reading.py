211d798dec5d14b2967891ef3c7f05b242003d64
Danny <dannypen@gmail.com>
2020-02-17 13:54:34 +0000

    Adding reading file

---
+++ reading.py
@@ -0,0 +1,16 @@
+file = open("teams.txt","w+")
+file.write("Liverpool \n")
+file.write("Manchester Utd \n")
+file.write("Manchester City \n")
+file.write("Real Madrid \n")
+file.write("PSG \n")
+file.seek(0)
+
+for i in range(5):
+    if i == 0 or i == 3:
+        print("Line %d: " %(i+1), file.readline())
+        file.readline()
+    else:
+        file.readline()
+        
+file.close
