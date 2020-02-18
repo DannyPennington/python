26614ec907b861201b3567444d968c0ff7ef46d5
Danny <dannypen@gmail.com>
2020-02-18 10:04:06 +0000

    Adding file

---
+++ debugging1.py
@@ -0,0 +1,9 @@
+user_funds = 10.31
+item_price = 1.50
+
+if item_price < user_funds:
+    print("You have enough money!")
+elif item_price == user_funds:
+    print("You have the precise amount of money")
+elif item_price > user_funds:
+    print("Sorry you don't have enough money")
